d231934299badb383b7229b64c7449964b5c6d75
Stephen Bussey <sb8244@cs.ship.edu>
2019-08-24 11:31:54 -0400

    Activity.create function

---
+++ lib/example_2/activity/activity_store.ex
@@ -0,0 +1,10 @@
+defmodule Example2.Activity.ActivityStore do
+  alias Example2.Repo
+  alias Example2.Activity.Activity
+
+  def create(params) do
+    %Activity{}
+    |> Activity.changeset(params)
+    |> Repo.insert()
+  end
+end
