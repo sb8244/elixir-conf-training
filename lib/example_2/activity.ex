e54fa9059d296d4aeabe64b4bb9aa628dac0a48d
Stephen Bussey <sb8244@cs.ship.edu>
2019-08-24 21:36:31 -0400

    After create, broadcast to the feed

--- lib/example_2/activity.ex
+++ lib/example_2/activity.ex
@@ -1,6 +1,16 @@
 defmodule Example2.Activity do
   alias __MODULE__.{ActivityStore}
 
-  defdelegate create(params), to: ActivityStore
   defdelegate all(params), to: ActivityStore
+
+  def create(params) do
+    case ActivityStore.create(params) do
+      ret = {:ok, activity} ->
+        Example2Web.Endpoint.broadcast("feed", "activity.created", activity)
+        ret
+
+      ret = _ ->
+        ret
+    end
+  end
 end
