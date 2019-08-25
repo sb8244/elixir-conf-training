3b99d68340e64f0c02ecf14d508e9813df5d38ce
Stephen Bussey <sb8244@cs.ship.edu>
2019-08-24 22:05:46 -0400

    Tie in for broadcasting

--- lib/example_2/activity.ex
+++ lib/example_2/activity.ex
@@ -6,11 +6,18 @@ defmodule Example2.Activity do
   def create(params) do
     case ActivityStore.create(params) do
       ret = {:ok, activity} ->
-        Example2Web.Endpoint.broadcast("feed", "activity.created", activity)
         ret
 
       ret = _ ->
         ret
     end
+  end
+
+  defp broadcast_activity(activity) do
+    # TODO
+    # You should add 1 line of code here to send the activity to the correct topic and
+    # message type. Look at `feed.js` to see what the topic is, and also what the message
+    # name is. You will want to broadcast the message data
+    Example2Web.Endpoint.broadcast("feed", "activity.created", activity)
   end
 end
